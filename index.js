7f905f40fdf9dc8e3d57ce25487489f74b65422e
Zainab Al-Najjar <eng.zainab.alnajar@gmail.com>
2024-02-23 01:50:07 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+// Tried to make the function more general by adding "start" & "destination" as parametars
+function distanceFromHqInBlocks(start , destination = 42){
+    if(start >= destination){
+        return start - destination;
+    } else {
+        return destination - start;
+    }
+}
+function distanceFromHqInFeet(start,destination){
+    const blocks = distanceFromHqInBlocks(start,destination);
+    return blocks * 264;
+}
+function distanceTravelledInFeet(start, destination){
+    const travelledInFeet = distanceFromHqInFeet(start, destination);
+    return travelledInFeet;
+}
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start,destination);
+    if(distance <= 400){
+        return 0;
+    } else if(distance <= 2000){
+        return (distance-400) * 0.02;
+    } else if(distance <= 2500){
+        return 25;
+    } else{
+        return "cannot travel that far"
+    }
+}
